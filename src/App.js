7a2320d6e6d26ec6c924147874964ca27df8cc70
Jake Thwaites <jakethwaites@hotmail.co.uk>
2019-03-07 21:23:42 +0000

    linked homepage component to RecipeBookContainer and app.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import RecipeBookContainer from './containers/RecipeBookContainer.js'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
-      </div>
+      <RecipeBookContainer />
     );
   }
 }
--- src/containers/RecipeBookContainer.js
+++ src/containers/RecipeBookContainer.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
-
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import HomePage from '../components/HomePage';
 
 class RecipeBookContainer extends Component {
 
@@ -13,29 +14,11 @@ class RecipeBookContainer extends Component {
         return (
            <Router>
               <Fragment>
-                 <NavBar />
-                 <Route exact path="/" component={HomePage} />
-                 <br></br>
-                 <br></br>
-                 {/* <Main /> */}
-                 {/* <Route path="/Explore" component={EnclosureList}/> */}
-                 <Route path="/Explore" render={() => <EnclosureList creaturesByEnclosure={this.state.enclosureArray} />}
-                 />
-                 {/* <Route path="/Region" component={RegionsDropDown} /> */}
-                 <Route path="/Region" render={() => <RegionsDropDown creatures={this.state.creaturesArray} />}
-                 />
-                 <Route path="/Search" component={SearchBox} />
-                 <Route path="/CreatureList" render={() => <CreatureList onRegionSelected={this.onRegionSelected} creatures={this.state.creaturesArray} />}
-                 />
-                 <br></br>
-                 <br></br>
-                 <Footer />
-                 <Route path="/FAQ" component={FAQMain} />
-                 <Route path="/AddCreature" component={AddCreature} />
-                 <Route path="/OurHistory" component={OurHistoryMain} />
-                 <Route path="/UsefulLinks" component={UsefulLinksMain} />
+                <Route exact path="/" component={HomePage} />
               </Fragment>
            </Router>
         )
      }
 }
+
+export default RecipeBookContainer;
