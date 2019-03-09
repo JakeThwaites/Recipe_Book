80c4f7771723e1b0009b836931c6779d182f5124
Jake Thwaites <jakethwaites@hotmail.co.uk>
2019-03-09 22:04:07 +0000

    created Main component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import RecipeBookContainer from './containers/RecipeBookContainer.js'
+import Main from './components/Main';
 import RecipeBox from './containers/RecipeBox';
 
 class App extends Component {
   render() {
     return (
-      <RecipeBox />
+      <Main />
     );
   }
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,2 +1,24 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import HomePage from './HomePage';
+import RecipeBox from '../containers/RecipeBox';
+
+class Main extends Component {
+  constructor() {
+    super();
+    this.state = { page: null };
+  }
+
+  render() {
+    return (
+      <Router>
+        <React.Fragment>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/recipes" component={RecipeBox} />
+        </React.Fragment>
+      </Router>
+    );
+  }
+}
+
+export default Main;
